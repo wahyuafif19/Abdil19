28648bc7c81e3ed103fb486b07f476b199a26feb
Wahyu Abdil Afif <145022805+wahyuafif19@users.noreply.github.com>
2024-06-11 11:59:54 +0700

    Add files via upload

---
+++ Prak SD JS 1/Latihan 1/Afif JS 1 Latihan 1.c
@@ -0,0 +1,10 @@
+// created by wahyu abdil afif_23343085
+
+
+#include <stdio.h>
+int main()
+{
+	printf("\n Welcome to the world of C ");// printf the message on the screen 
+	return 0;// returns a value 0 to the operating system 
+	
+}
---
+++ Prak SD JS 1/Tugas 1/Afif Job 1 Introduction.c
@@ -0,0 +1,8 @@
+// created by wahyu abdil afif_23343085
+
+#include <stdio.h>
+int main()
+{
+	printf("\nWelcome to the world of C"); //prints the message on the screen
+	return 0; //returns a value 0 to the operating system
+}
